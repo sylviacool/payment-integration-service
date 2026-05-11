5c3d6282cb492730fb6c542c2e27a84329e5ab5a
Ogechi Sylvia Uzoma <sylviacool95@gmail.com>
2026-05-11 11:02:39 +0200

    Initial Spring Boot project setup

---
+++ src/main/java/com/paymentintegration/PaymentIntegrationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.paymentintegration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentIntegrationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaymentIntegrationServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/paymentintegration/PaymentIntegrationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paymentintegration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentIntegrationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
