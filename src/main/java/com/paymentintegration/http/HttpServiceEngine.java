1d18a9f6bff646034a16d1fc512f0c071e91e508
Ogechi Sylvia Uzoma <sylviacool95@gmail.com>
2026-05-11 12:34:33 +0200

    Implement centralized HTTP communication engine

--- src/main/java/com/paymentintegration/http/HttpServiceEngine.java
+++ src/main/java/com/paymentintegration/http/HttpServiceEngine.java
@@ -2,6 +2,7 @@ package com.paymentintegration.http;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestClient;
 
@@ -12,4 +13,30 @@ public class HttpServiceEngine {
 
     private final RestClient restClient;
 
+    public ResponseEntity<String> makeHttpCall(HttpRequest httpRequest) {
+
+        log.info("Making HTTP call to: {}", httpRequest.getUri());
+
+        try {
+
+            ResponseEntity<String> response = restClient
+                    .method(httpRequest.getHttpMethod())
+                    .uri(httpRequest.getUri())
+                    .headers(headers ->
+                            headers.addAll(httpRequest.getHttpHeaders()))
+                    .body(httpRequest.getBody())
+                    .retrieve()
+                    .toEntity(String.class);
+
+            log.info("HTTP call successful. Status: {}", response.getStatusCode());
+
+            return response;
+
+        } catch (Exception e) {
+
+            log.error("Error during HTTP call", e);
+
+            throw new RuntimeException("HTTP communication failed", e);
+        }
+    }
 }
