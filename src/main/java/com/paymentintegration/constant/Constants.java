3d5f42fb667ccb57227510dec849917985a28a31
Ogechi Sylvia Uzoma <sylviacool95@gmail.com>
2026-05-11 13:05:46 +0200

    Add PayPal configuration properties and OAuth constants

---
+++ src/main/java/com/paymentintegration/constant/Constants.java
@@ -0,0 +1,8 @@
+package com.paymentintegration.constant;
+
+public class Constants {
+
+    public static final String CLIENT_CREDENTIALS = "client_credentials";
+
+    public static final String GRANT_TYPE = "grant_type";
+}
