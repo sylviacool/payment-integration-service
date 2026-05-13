c2f952f259bdbbe7db7c0e1da50e2371658784e1
Ogechi Sylvia Uzoma <sylviacool95@gmail.com>
2026-05-13 21:20:05 +0200

    Add create order REST controller endpoint

---
+++ src/main/java/com/paymentintegration/controller/OrderController.java
@@ -0,0 +1,32 @@
+package com.paymentintegration.controller;
+
+import com.paymentintegration.dto.request.CreateOrderReq;
+import com.paymentintegration.dto.response.OrderRes;
+import com.paymentintegration.service.interfaces.OrderService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/v1/orders")
+@RequiredArgsConstructor
+@Slf4j
+public class OrderController {
+
+    private final OrderService orderService;
+
+    @PostMapping
+    public ResponseEntity<OrderRes> createOrder(
+            @RequestBody CreateOrderReq createOrderReq
+    ) {
+
+        log.info("Create order request received: {}",
+                createOrderReq);
+
+        OrderRes response =
+                orderService.createOrder(createOrderReq);
+
+        return ResponseEntity.ok(response);
+    }
+}
