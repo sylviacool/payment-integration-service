c00695ca8fa5816ba1263c301dc17c81c0240363
Ogechi Sylvia Uzoma <sylviacool95@gmail.com>
2026-05-14 22:12:02 +0200

    Add provider entity mapping

---
+++ src/main/java/com/paymentintegration/entity/ProviderEntity.java
@@ -0,0 +1,16 @@
+package com.paymentintegration.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Table(name = "provider")
+@Data
+public class ProviderEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String providerName;
+}
