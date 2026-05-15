68a959a0149ca4098f189f2fc2d05f55976fa73f
Ogechi Sylvia Uzoma <sylviacool95@gmail.com>
2026-05-15 10:52:49 +0200

    Add transaction repository for payment persistence

---
+++ src/main/java/com/paymentintegration/repository/TransactionRepository.java
@@ -0,0 +1,15 @@
+package com.paymentintegration.repository;
+
+import com.paymentintegration.entity.TransactionEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface TransactionRepository
+        extends JpaRepository<TransactionEntity, Long> {
+
+    Optional<TransactionEntity> findByTxnReference(String txnReference);
+
+    Optional<TransactionEntity> findByProviderReference(String providerReference);
+
+}
