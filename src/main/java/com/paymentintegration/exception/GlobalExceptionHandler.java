6c3f6f25e923e6033bf56309151cf4b2c507ba90
Ogechi Sylvia Uzoma <sylviacool95@gmail.com>
2026-05-16 12:38:59 +0200

    Add Spring Security dependency

--- src/main/java/com/paymentintegration/exception/GlobalExceptionHandler.java
+++ src/main/java/com/paymentintegration/exception/GlobalExceptionHandler.java
@@ -1,14 +1,15 @@
 package com.paymentintegration.exception;
 
 import com.paymentintegration.dto.response.ErrorResponse;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-import java.util.List;
 
+@Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
@@ -65,6 +66,25 @@ public class GlobalExceptionHandler {
         return new ResponseEntity<>(
                 errorResponse,
                 HttpStatus.NOT_FOUND
+        );
+    }
+
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponse>
+    handleGeneralException(Exception ex) {
+
+        log.error("Unexpected error occurred", ex);
+
+        ErrorResponse errorResponse =
+                new ErrorResponse(
+                        HttpStatus.INTERNAL_SERVER_ERROR.value(),
+                        "An unexpected error occurred"
+                );
+
+        return new ResponseEntity<>(
+                errorResponse,
+                HttpStatus.INTERNAL_SERVER_ERROR
         );
     }
 }
