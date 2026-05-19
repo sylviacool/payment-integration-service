8103f1484f883f255ca353de8e736b07eea3f47a
Ogechi Sylvia Uzoma <sylviacool95@gmail.com>
2026-05-19 10:51:25 +0200

    Implement PayPal webhook event handling and Extract PayPal order ID from webhook payload

--- src/main/java/com/paymentintegration/controller/WebhookController.java
+++ src/main/java/com/paymentintegration/controller/WebhookController.java
@@ -1,5 +1,7 @@
 package com.paymentintegration.controller;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -9,6 +11,8 @@ import org.springframework.web.bind.annotation.*;
 @Slf4j
 public class WebhookController {
 
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
     @PostMapping("/paypal")
     public ResponseEntity<String> handlePaypalWebhook(
             @RequestBody String payload,
@@ -16,10 +20,44 @@ public class WebhookController {
             String transmissionId
     ) {
 
-        log.info("PayPal Webhook Received");
-        log.info("Transmission ID: {}", transmissionId);
-        log.info("Payload: {}", payload);
+        try {
+            log.info("PayPal Webhook Received");
+            log.info("Transmission ID: {}", transmissionId);
 
-        return ResponseEntity.ok("Webhook received");
+            JsonNode root =
+                    objectMapper.readTree(payload);
+
+            String eventType =
+                    root.get("event_type").asText();
+
+            log.info("Event Type: {}", eventType);
+
+            String paypalOrderId =
+                    root
+                            .get("resource")
+                            .get("id").asText();
+
+            log.info("PayPal Order ID: {}", paypalOrderId); //maps to provider_reference in DB
+
+            switch (eventType) {
+                case "PAYMENT.CAPTURE.COMPLETED":
+                    log.info("Payment completed successfully");
+                    break;
+
+                case "PAYMENT.CAPTURE.DENIED":
+                    log.info("Payment was denied");
+                    break;
+
+                default:
+                    log.info("Unhandled webhook event");
+            }
+
+            return ResponseEntity.ok("Webhook processed successfully");
+
+        } catch (Exception ex) {
+            log.error("Webhook processing failed", ex);
+            return ResponseEntity.badRequest()
+                    .body("Webhook processing failed");
+        }
     }
 }
